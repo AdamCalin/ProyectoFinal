2c4bddf181e2cbafa76b4fd13cba1a193430edc5
AdamCalin <acalinwork@gmail.com>
2022-06-10 14:42:15 +0200

    Tabla Stock

--- src/app/components/contenido/gestion/pedidos-stock/pedidos-stock.component.ts
+++ src/app/components/contenido/gestion/pedidos-stock/pedidos-stock.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, Input, NgModule } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import { UsuarioService } from 'src/app/services/usuarios/usuarios.service';
 import { trigger, animate, transition, style, state } from '@angular/animations';
+import Swal from 'sweetalert2';
+import { AppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import * as FormActions from '../../../../shared/components/state/usuario-gestion/usuario-gestion.action';
+import { StockService } from '../../../../services/stock/stock.service';
+
 
 @Component({
   selector: 'app-pedidos-stock',
@@ -17,18 +26,92 @@ import { trigger, animate, transition, style, state } from '@angular/animations'
     ])
   ]
 })
-export class PedidosStockComponent implements OnInit {
+export class PedidosStockComponent implements AfterViewInit {
 
-  showModal: boolean = false;
+  displayedColumns: string[] = ['Articulo', 'Color','Talla','Cantidad_stock', 'Cantidad_pedido', 'Cantidad_envio', 'edit', 'delete'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  //variables from crear nuevo usuarios
+  showModalAdd: boolean = false;
+  showModalEdit: boolean = false;
+
   fadeOut: boolean = false;
-  
-  constructor() {
-   }
 
-  ngOnInit() {
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  idUsuario : any;
+
+  @Input() id = '';
+
+  constructor(private servicioStock: StockService, private store: Store<AppState>){
+
   }
+  ngAfterViewInit() {
+    this.datosVistaStock();
+    this.dataSource.paginator = this.paginator;
+  }
+
+  datosVistaStock(){
+    this.servicioStock.getVistaStock().subscribe( (articulos: any) => {
+      // console.log(articulos);
+      this.dataSource.data = articulos;
+      
+    })
+  }
+  confirmBorrarUsuario(id :any){
+    
+    Swal.fire({
+      title: 'Advertencia!!',
+      text: "¿Estas seguro de que quieres Borrar ese Usuario?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, estoy seguro!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+          // this.borrarUsuario(id);
+        Swal.fire(
+          'Usuario borrado Correctamente!',
+        )
+      }
+    })
+  }
+  // borrarUsuario(id : any){    
+  //   this.servicioStock.deleteArticulo(id).subscribe( (res : any) => {
+  //     // console.log(res);
+  //     this.datosVistaStock();
+  //   })
+  // }
+ 
   setClose($event: any) {
-    this.showModal = false;
+    this.showModalAdd = false;
+    this.showModalEdit = false;
+    this.store.dispatch( FormActions.unSetForm());
   }
+
+  actualizarDatos($event : any){
+    if($event == true){
+    }
+  }
+  filter(event : any){
+      let filter = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filter.trim().toUpperCase();
+  }
+
 
 }
+
+export interface PeriodicElement {
+ Articulo: string,
+ Color: string,
+ Talla: string,
+ Cantidad_stock: number,
+ Cantidad_pedido: number,
+ Cantidad_envio: number
+}
+
+let  ELEMENT_DATA: PeriodicElement[] = [
+]
+
+
---
+++ src/app/services/stock/stock.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  dataLogin: any = null;
+
+  url:string = "http://localhost:7187/api";
+
+
+  constructor(private http: HttpClient, private store:Store<AppState>) {
+    this.store.select('login').subscribe(state => {
+      this.dataLogin = state.token; 
+    });
+   }
+
+   headers() {
+     return new HttpHeaders({
+      'Authorization': `Bearer ${this.dataLogin.token}`
+     })
+  }
+
+  getVistaStock(){
+
+    let headers = this.headers();
+    return this.http.get(`${this.url}/stock`, {headers});
+  }
+  deleteArticulo(){
+      
+  }
+}
